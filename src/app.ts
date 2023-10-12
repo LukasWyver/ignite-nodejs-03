2f5cc2780a706eb8bfb9708e1af3ee979d525bb3
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-10-12 19:06:28 -0300

    setup database serverless neondb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import fastify from 'fastify'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
+import ws from 'ws'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+// Setup
+neonConfig.webSocketConstructor = ws
+const connectionString = `${env.DATABASE_URL}`
 
+// Init prisma client
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+const prisma = new PrismaClient({ adapter })
+
+// Use Prisma Client as normal
 prisma.user.create({
   data: {
     name: 'Lucas',
