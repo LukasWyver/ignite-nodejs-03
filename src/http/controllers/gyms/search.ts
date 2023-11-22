89822a33cf0cbcf30198bcc5d5ed3ce53c469073
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-11-21 22:50:34 -0300

    error: expected 1 but received 2 in nearby.test

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -4,20 +4,20 @@ import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-us
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
   const searchGymsQuerySchema = z.object({
-    query: z.string(),
+    q: z.string(),
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { query, page } = searchGymsQuerySchema.parse(request.query)
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
 
   const searchGymsUseCase = makeSearchGymsUseCase()
 
   const { gyms } = await searchGymsUseCase.execute({
-    query,
+    query: q,
     page,
   })
 
-  return reply.status(201).send({
+  return reply.status(200).send({
     gyms,
   })
 }
